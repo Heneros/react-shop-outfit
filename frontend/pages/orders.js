98812be6824cd86b260010cc4495a23b21f49a1c
Heneros <punk60826@gmail.com>
2022-03-06 02:01:59 +0200

    signIn page created and User.js

--- frontend/pages/orders.js
+++ frontend/pages/orders.js
@@ -4,9 +4,7 @@ import Pagination from '../components/Pagination';
 export default function OrderPage() {
   return (
     <div>
-      <Pagination page={1} />
-      <Products />
-      <Pagination page={1}  />
+      <p>Orders page</p>
     </div>
   );
 }

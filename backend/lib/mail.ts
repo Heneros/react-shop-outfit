ab36fb96b99a9b882b0212068bb7a3f40e94da2f
Heneros <punk60826@gmail.com>
2022-03-13 05:11:13 +0200

    fixed issue with send email

--- backend/lib/mail.ts
+++ backend/lib/mail.ts
@@ -1,11 +1,15 @@
-import { createTransport } from "nodemailer";
+import { createTransport, getTestMessageUrl } from "nodemailer";
 
 const transport = createTransport({
     host: process.env.MAIL_HOST,
     port: process.env.MAIL_PORT,
+    secure: false,
     auth:{
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
@@ -25,6 +29,16 @@ function makeANiceEmail(text: string): string{
     </div>
     `
 }
+export interface MailResponse {
+    accepted?: (string)[] | null;
+    rejected?: (null)[] | null;
+    envelopeTime: number;
+    messageTime: number;
+    messageSize: number;
+    response: string;
+    envelope: Envelope;
+    messageId: string;
+  }
 interface MailResponse{
     message: string;
 }
@@ -40,4 +54,7 @@ export async function sendPasswordResetEmail(resetToken: string, to: string): Pr
          </a>
         `),
     })) as MailResponse;
+    if(process.env.MAIL_USER.includes('ethereal.email')){
+        console.log(`Message Sent:  ${getTestMessageUrl(info)}`);
+    }
 }

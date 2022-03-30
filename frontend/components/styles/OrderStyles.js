922790a8395aa667183d5058b7a15307c3fd2786
Heneros <punk60826@gmail.com>
2022-03-30 03:17:49 +0300

    orders/ display data on page

--- frontend/components/styles/OrderStyles.js
+++ frontend/components/styles/OrderStyles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-
+ 
 const OrderStyles = styled.div`
   max-width: 1000px;
   margin: 0 auto;
@@ -19,6 +19,33 @@ const OrderStyles = styled.div`
         text-align: right;
       }
     }
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+  .images{
+    display: grid;
+    grid-template-columns: repeat(auto-fit,minmax(0,1fr));
+    grid-gap: 10px;
+    margin-top: 1rem;
+    img{
+      width: 100%;
+      height: 350px;
+      object-fit: cover; 
+    }
+
+  }
+  .items{
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+    grid-gap: 4rem;
+    img{
+      width: 100%;
+      height: 350px;
+      object-fit: cover; 
+    }
   }
   .order-item {
     border-bottom: 1px solid var(--offWhite);
@@ -28,11 +55,7 @@ const OrderStyles = styled.div`
     grid-gap: 2rem;
     margin: 2rem 0;
     padding-bottom: 2rem;
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
+ 
   }
 `;
 export default OrderStyles;
